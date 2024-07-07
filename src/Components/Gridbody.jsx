e887f92ae01c719f31a7fdbc3d14d73d36eb674d
rahuwul <rahuwul@gmail.com>
2024-07-07 12:03:18 +0530

    added sampleoutput.json

--- src/Components/Gridbody.jsx
+++ src/Components/Gridbody.jsx
@@ -1,35 +1,19 @@
 import React from 'react'
-
+import Card from './Card'
+import data from '../SampleOutput.json'
 
 export default function Gridbody() {
-  const data={
-    title:"Lorem Ipsum Dolor",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the...",
-    date:"22th July 2024",
-    source:"Times Of India",
-
-  }
+  
   return (
     <>
-   <div className='flex-1 bg-[#171717] max-lg:ml-[18rem] max-md:ml-[16rem] max-sm:ml-[1rem] ml-[22rem] mr-4 mt-24 overflow-y-auto'>
-   <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-4 '>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    <div className='bg-red-500 h-72'>.</div>
-    </div>
-   </div>
+<div className='flex justify-center items-center  max-lg:ml-[18rem] max-md:ml-[16rem] max-sm:ml-[3rem] ml-[22rem] mr-4 mt-24 overflow-y-auto'>
+  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-4 mx-auto'>
+  {data.articles.map((item, index) => (
+        <Card key={index} title={item.title} description={item.description} source={item.source.name} date={item.publishedAt} />
+      ))}
+  </div>
+</div>
+
     </>
   )
 }
